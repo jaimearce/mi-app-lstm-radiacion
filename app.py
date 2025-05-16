6f045676d0ae19a3c7bd97ce228ac5189fb33791
jaimearce <81883118+jaimearce@users.noreply.github.com>
2025-05-15 21:17:24 -0500

    Update app.py
    add mejora de fondo

--- app.py
+++ app.py
@@ -28,7 +28,9 @@ def set_custom_style():
             background-color: #f5f9ff;
         }}
         .stApp {{
-            background-image: url("https://raw.githubusercontent.com/jaimearce/mi-app-lstm-radiacion/main/fondo.jpg");
+            position: relative;
+            background: linear-gradient(rgba(150, 200, 150, 0.45), rgba(150, 200, 150, 0.45)), 
+                        url("https://raw.githubusercontent.com/jaimearce/mi-app-lstm-radiacion/main/fondo.jpg");
             background-size: cover;
             background-position: center;
             background-repeat: no-repeat;
